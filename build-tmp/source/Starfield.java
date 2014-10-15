ccd17a9b31c094af92fc0da44687a9ee3476869c
PhilipHuang2 <philiphuang2@gmail.com>
2014-10-15 12:35:24 -0700

    Basic Code

--- build-tmp/source/Starfield.java
+++ build-tmp/source/Starfield.java
@@ -1,10 +1,26 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Starfield extends PApplet {
+
 NormalParticle philip = new NormalParticle();
-void setup()
+public void setup()
 {
 	size(400,400);
 	background(0);
 }
-void draw()
+public void draw()
 {
 	background(0);
 	philip.show();
@@ -28,19 +44,19 @@ class NormalParticle
  	 	angle = (Math.random()*2*PI);
 
 	}
-	void move()
+	public void move()
 	{
 		posX = posX + speed * Math.cos(angle);
 		posY = posY + speed * Math.sin(angle);
 	}
 
-	void show()
+	public void show()
 	{
 		fill(colour[0],colour[1],colour[2]);
 		ellipse((int)(posX),(int)(posY),10,10);
 	}
 
-	void reWind()
+	public void reWind()
 	{
  		if(Math.sqrt(Math.pow(posX-200,2) + Math.pow(posY-200,2)) >= 200)
  		{
@@ -67,3 +83,12 @@ class OddballParticle
 }
 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Starfield" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
